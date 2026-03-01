1454fb545daa423b90e46c4ec14e061ef62d78b8
fpatron <francisj.patron@gmail.com>
2026-03-01 08:54:20 -0700

    Closure dispatch table replaced with a switch state statement

--- jsonc.go
+++ jsonc.go
@@ -4,13 +4,12 @@ import (
 	"encoding/json"
 )
 
-// Unmarshal parses the JSON-encoded data with comment support and stores the result in the value pointed to by v.
 func Unmarshal(data []byte, v interface{}) error {
-	processedData := decomment(data)
+	processedData := stripComments(data)
 	return json.Unmarshal(processedData, v)
 }
 
-func decomment(data []byte) []byte {
+func stripComments(data []byte) []byte {
 	const (
 		OUTSIDE = iota
 		SINGLE_LINE
@@ -23,65 +22,48 @@ func decomment(data []byte) []byte {
 	result := make([]byte, len(data))
 	copy(result, data)
 
-	stateHandlers := []func(int, []byte, []byte) int{
-		// OUTSIDE
-		func(i int, data, result []byte) int {
+	for i := 0; i < len(result); i++ {
+		switch state {
+		case OUTSIDE:
 			if data[i] == '/' && i+1 < len(data) {
 				if data[i+1] == '/' {
 					state = SINGLE_LINE
 					result[i] = ' '
 					result[i+1] = ' '
-					return i + 1 // skip the next character
+					i++
 				} else if data[i+1] == '*' {
 					state = MULTI_LINE
 					result[i] = ' '
 					result[i+1] = ' '
-					return i + 1 // skip the next character
+					i++
 				}
 			} else if data[i] == '"' {
 				state = IN_STRING
 			}
-			return i
-		},
-		// SINGLE_LINE
-		func(i int, data, result []byte) int {
+		case SINGLE_LINE:
 			if data[i] == '\n' {
 				state = OUTSIDE
 			} else {
 				result[i] = ' '
 			}
-			return i
-		},
-		// MULTI_LINE
-		func(i int, data, result []byte) int {
+		case MULTI_LINE:
 			if data[i] == '*' && i+1 < len(result) && data[i+1] == '/' {
 				state = MULTI_LINE_ENDING
 				result[i] = ' '
 				result[i+1] = ' '
-				return i + 1 // skip the next character
+				i++
 			} else if result[i] != '\n' {
 				result[i] = ' '
 			}
-			return i
-		},
-		// MULTI_LINE_ENDING
-		func(i int, data, result []byte) int {
+		case MULTI_LINE_ENDING:
 			state = OUTSIDE
-			return i
-		},
-		// IN_STRING
-		func(i int, data, result []byte) int {
+		case IN_STRING:
 			if data[i] == '\\' && i+1 < len(data) {
-				return i + 1 // skip the escaped character
+				i++
 			} else if data[i] == '"' {
 				state = OUTSIDE
 			}
-			return i
-		},
-	}
-
-	for i := 0; i < len(result); i++ {
-		i = stateHandlers[state](i, data, result)
+		}
 	}
 
 	return result
